f049a8f907cb029f128c1379736ce44548bdf868
dsudzeng <38187550+dsudzeng@users.noreply.github.com>
2024-04-11 14:31:34 -0500

    Update image_app.py

--- image_app.py
+++ image_app.py
@@ -3,10 +3,15 @@ import os
 import webbrowser
 
 import streamlit as st
+st.set_page_config(layout="wide")
 
-st.image(image="DSU_Logo.png", width=120)
+st.image('DSU_Logo.tif', width=750)
 
-st.header('ARTIFICIAL INTELLIGENCE IN ORGANIZATIONS (BS) ~ The future of business', divider='rainbow')
+st.header('ARTIFICIAL INTELLIGENCE IN ORGANIZATIONS (BS)', divider='rainbow')
+
+new_title = '<p style="font-family:sans-serif; color:Green; font-size: 30px;">The future of business</p>'
+st.markdown(new_title, unsafe_allow_html=True)
+
 
 st.write('**Students will learn to understand what artificial intelligence is, and what AI means for businesses and organizations and develop skills for applying artificial intelligence tools and methods in a variety of industries. With these skills, they will be prepared to help make data-driven decisions in their companies, optimize business processes and workflows, minimize costs, and maximize revenues by utilizing AI tools and applications. They will also be able to successfully navigate AI-related issues, challenges, and ethics in workplaces and be AI-aware in society.**')
 
@@ -14,8 +19,6 @@ st.title('🦜🔗 Image Generator')
 
 prompt_input = st.text_input('Please enter a prompt in the textbox')
  
-os.environ['OPENAI_API_KEY'] = ""
-
 if prompt_input:
     client = OpenAI()
 

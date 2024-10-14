6da104ec04e471bc28f0ef0ccb5276f3ca926179
dsudzeng <38187550+dsudzeng@users.noreply.github.com>
2024-10-14 17:09:05 -0500

    Update image_app.py

--- image_app.py
+++ image_app.py
@@ -5,15 +5,30 @@ import requests
 import streamlit as st
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 st.set_page_config(layout="wide")
-st.image('DSU_Logo.tif', width=750)
-st.header('ARTIFICIAL INTELLIGENCE IN ORGANIZATIONS (BS)', divider='rainbow')
-new_title = '<p style="font-family:sans-serif; color:Green; font-size: 30px;">The future of business</p>'
-st.markdown(new_title, unsafe_allow_html=True)
+#st.image('DSU_Logo.tif', width=750)
+#st.header('ARTIFICIAL INTELLIGENCE IN ORGANIZATIONS (BS)', divider='rainbow')
+#new_title = '<p style="font-family:sans-serif; color:Green; font-size: 30px;">The future of business</p>'
+#st.markdown(new_title, unsafe_allow_html=True)
 
 
-st.write('**Students will learn to understand what artificial intelligence is, and what AI means for businesses and organizations and develop skills for applying artificial intelligence tools and methods in a variety of industries. With these skills, they will be prepared to help make data-driven decisions in their companies, optimize business processes and workflows, minimize costs, and maximize revenues by utilizing AI tools and applications. They will also be able to successfully navigate AI-related issues, challenges, and ethics in workplaces and be AI-aware in society.**')
+#st.write('**Students will learn to understand what artificial intelligence is, and what AI means for businesses and organizations and develop skills for applying artificial intelligence tools and methods in a variety of industries. With these skills, they will be prepared to help make data-driven decisions in their companies, optimize business processes and workflows, minimize costs, and maximize revenues by utilizing AI tools and applications. They will also be able to successfully navigate AI-related issues, challenges, and ethics in workplaces and be AI-aware in society.**')
 
-st.title('🦜🔗 Image Generator')
+st.title('🦜🔗 Paradigm Futures Image Generator')
+
+# Pre-determined password
+PASSWORD = "paradigmfutures"
+
+# Instructions for the user
+st.write("Please enter the password to enable the submit button.")
+
+# Password input field
+entered_password = st.text_input("Enter Password", type="password")
+
+# Disable the submit button unless the correct password is entered
+if entered_password == PASSWORD:
+    submit_button = 1
+else:
+    st.error("Enter the correct password to enable the submit button.")
 
 prompt_input = st.text_input('Please enter a prompt in the textbox')
 
@@ -27,7 +42,7 @@ def nav_to(url, prompt_input):
         st.write(nav_script, unsafe_allow_html=True)
         st.write(prompt_input)
 
-if prompt_input:
+if prompt_input and submit_button:
     client = OpenAI()
 
     response = client.images.generate(

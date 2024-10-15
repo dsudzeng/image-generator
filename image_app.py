2cd1d6467c1a711731f8156dec56acfcbcf29976
dsudzeng <38187550+dsudzeng@users.noreply.github.com>
2024-10-15 09:07:02 -0500

    Update image_app.py

--- image_app.py
+++ image_app.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="wide")
 st.image('DSU_Logo.tif', width=750)
 st.header('ARTIFICIAL INTELLIGENCE IN ORGANIZATIONS (BS)', divider='rainbow')
 new_title = '<p style="font-family:sans-serif; color:Green; font-size: 30px;">The future of business</p>'
-#st.markdown(new_title, unsafe_allow_html=True)
+st.markdown(new_title, unsafe_allow_html=True)
 
 
 st.write('**Students will learn to understand what artificial intelligence is, and what AI means for businesses and organizations and develop skills for applying artificial intelligence tools and methods in a variety of industries. With these skills, they will be prepared to help make data-driven decisions in their companies, optimize business processes and workflows, minimize costs, and maximize revenues by utilizing AI tools and applications. They will also be able to successfully navigate AI-related issues, challenges, and ethics in workplaces and be AI-aware in society.**')
